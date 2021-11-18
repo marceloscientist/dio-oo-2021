ce3fd8bf51866a196c85ab4ed5f1e185d42c04fb
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-11-17 22:25:11 -0300

    Inicio da implementacao da conta bancaria

---
+++ src/Conta.java
@@ -0,0 +1,45 @@
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaATransferir) {
+        this.sacar(valor);
+        contaATransferir.depositar(valor);
+    }
+
+    // getters e setters
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+}
